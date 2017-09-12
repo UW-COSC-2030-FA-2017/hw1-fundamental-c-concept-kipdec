92c26cdcdd8b7bb8bd26736cae08dcd0e21c18f1
kipdec <kip@kips-mbp.uwyo.edu>
2017-09-12 10:43:36 -0600

    Added comments

--- parameterpassingdemo.cpp
+++ parameterpassingdemo.cpp
@@ -1,7 +1,7 @@
 //
 //  parameterpassingdemo.cpp
-//  
-//
+//  This program demonstrates how parameter passing works through pass by value
+//  and pass by reference.
 //  Created by Kip DeCastro on 9/12/17.
 //
 //
@@ -11,20 +11,20 @@ using std::cout;
 using std::cin;
 using std::endl;
 
-void passByValue(int someVar);
-void passByReference(int& someVar);
+void passByValue(int someVar); 			// Our function to pass by value
+void passByReference(int& someVar);		// Our function to pass by reference.
 
 int main(){
 	int ourVar;
     cout << "What do we want our integer variable to be? ";
     cin >> ourVar;
     cout << "Our variable is " << ourVar << ", now lets see what happens when " <<
-    	"we pass by value..." << endl;
+    	"we pass by value..." << endl; 	// ourVar will not change here.
     passByValue(ourVar);
     cout << "Our variable is now " << ourVar << ". \nNow lets pass by reference!"
     	<< endl;
     passByReference(ourVar);
-    cout << "Our variable is now " << ourVar << endl;
+    cout << "Our variable is now " << ourVar << endl; // ourVar will now change.
 }
 
 void passByValue(int someVar){
